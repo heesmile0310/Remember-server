e87333506d144cbdcd6c28f60756c9b1e9444132
Lee Joo Hyung <wngud985@gmail.com>
2021-06-30 12:03:17 +0900

    [server] feat/routes add router

---
+++ src/routes/login.ts
@@ -0,0 +1,8 @@
+const express = require("express");
+const loginRouter = express.Router();
+
+const { loginController } = require("../controllers/loginController");
+
+loginRouter.post("/login", loginController);
+
+export default loginRouter;
