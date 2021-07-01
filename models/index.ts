5c39655887967a4eaedba53875b2cbcec63e0127
Kimseonghee <kimsunghee625@gmail.com>
2021-07-01 23:58:53 +0900

    [server] setting/sequelize final test

--- models/index.ts
+++ models/index.ts
@@ -1,22 +1,28 @@
+"use strict";
+
+import { Sequelize } from "sequelize";
+const env = "development";
+import { Config } from "../config/config";
+const config = Config[env];
+console.log(config);
+
+export const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  {
+    host: config.host,
+    port: config.port,
+    dialect: config.dialect,
+  }
+);
 // import { Sequelize } from "sequelize";
 // import { config } from "../config/config";
 
-// export const sequelize = new Sequelize(
-//   config.development.database,
-//   config.development.username,
-//   config.development.password,
-//   {
-//     host: config.development.host,
-//     dialect: "mysql",
-//   }
-// );
-import { Sequelize } from "sequelize";
-import { config } from "../config/config";
-
-export const sequelize = new Sequelize("test", "root", "", {
-  host: "localhost",
-  dialect: "mysql",
-});
+// export const sequelize = new Sequelize("test", "root", "", {
+//   host: "localhost",
+//   dialect: "mysql",
+// });
 // 'use strict';
 
 // const fs = require('fs');
