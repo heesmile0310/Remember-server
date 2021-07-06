4d606687ade8f601950d85af42528a847cd11d39
Kimseonghee <kimsunghee625@gmail.com>
2021-07-06 13:19:52 +0900

    [server] fix/comment fix comment add url

--- controllers/commentController.ts
+++ controllers/commentController.ts
@@ -3,25 +3,27 @@ import { Comments } from "../models/Comments";
 import { Users } from "../models/Users";
 import { MainPosts } from "../models/MainPosts";
 import { create } from "domain";
+import { url } from "inspector";
 
 const commentController = {
   commentController: async (req: Request, res: Response) => {
-    const { name, comment, post_title } = req.body;
+    const { name, comment, post_title, url } = req.body;
     await Comments.create({
       user_id: null,
       user_name: name,
       mainPost_id: null,
       post_title: post_title,
       comment: comment,
+      url: url,
     });
   },
 
   commentListController: async (req: Request, res: Response) => {
-    const { post_title } = req.body;
+    const { post_title, user_name } = req.body;
     const commentInfo = await Comments.findAll({
       where: { post_title: post_title },
     });
-    res.status(200).send({ data: commentInfo });
+    res.status(200).send({ data: { commentInfo } });
   },
 };
 
