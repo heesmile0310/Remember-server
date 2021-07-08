c3e9f8b0a277260822cbebbe55deb0e4bcdb3fb1
Kimseonghee <kimsunghee625@gmail.com>
2021-07-08 16:32:24 +0900

    [server] fix/deployerror fix deploy error

--- models/MainPosts.ts
+++ models/MainPosts.ts
@@ -3,23 +3,23 @@ import { DataTypes, Model, Association } from "sequelize";
 import { sequelize } from "./index";
 
 interface MainPostAttributes {
-  title: string; //제목
-  casualty: string; //희생자수
-  date: string; //발생 일자
-  location: string; //발생 위치
-  body: string; //본문 내용 (자세히 보기 후 사용)
-  url: string; // 이미지 url
-  targetAmount: number; // 후원 목표액 (값이 달라질 수 있음)
+  title: string | null; //제목
+  casualty: string | null; //희생자수
+  date: string | null; //발생 일자
+  location: string | null; //발생 위치
+  body: string | null; //본문 내용 (자세히 보기 후 사용)
+  url: string | null; // 이미지 url
+  targetAmount: number | null; // 후원 목표액 (값이 달라질 수 있음)
 }
 
 export class MainPosts extends Model<MainPostAttributes> {
-  public title!: string;
-  public casualty!: string;
-  public date!: string;
-  public location!: string;
-  public body!: string;
-  public url!: string;
-  public targetAmount!: number;
+  public title!: string | null;
+  public casualty!: string | null;
+  public date!: string | null;
+  public location!: string | null;
+  public body!: string | null;
+  public url!: string | null;
+  public targetAmount!: number | null;
 }
 
 MainPosts.init(
--- models/Users.ts
+++ models/Users.ts
@@ -6,23 +6,23 @@ import { DataTypes, Model, Association } from "sequelize";
 import { sequelize } from "./index";
 
 interface UserAttributes {
-  name: string;
-  email: string;
-  password: string;
-  mobile: string;
-  dateOfBirth: string;
-  OAuth: boolean;
-  url: string;
+  name: string | null;
+  email: string | null;
+  password: string | null;
+  mobile: string | null;
+  dateOfBirth: string | null;
+  OAuth: boolean | null;
+  url: string | null;
 }
 
 export class Users extends Model<UserAttributes> {
-  public name!: string;
-  public email!: string;
-  public password!: string;
-  public mobile!: string;
-  public dateOfBirth!: string;
-  public OAuth!: boolean;
-  public url!: string;
+  public name!: string | null;
+  public email!: string | null;
+  public password!: string | null;
+  public mobile!: string | null;
+  public dateOfBirth!: string | null;
+  public OAuth!: boolean | null;
+  public url!: string | null;
 }
 
 Users.init(
