5c5b0172faa379c166e1b684abd7b42195c9fb4e
Kimseonghee <kimsunghee625@gmail.com>
2021-07-08 13:18:23 +0900

    [server] fix/quizzestable fix quiz table column

--- models/Quizzes.ts
+++ models/Quizzes.ts
@@ -4,15 +4,24 @@ import { sequelize } from "./index";
 
 interface QuizAttributes {
   quiz: string;
+  correct: string;
+  wrong: string;
+  boolean: boolean;
 }
 
 export class Quizzes extends Model<QuizAttributes> {
   public quiz!: string;
+  public correct!: string;
+  public wrong!: string;
+  public boolean!: boolean;
 }
 
 Quizzes.init(
   {
     quiz: DataTypes.STRING,
+    correct: DataTypes.STRING,
+    wrong: DataTypes.STRING,
+    boolean: DataTypes.TINYINT,
   },
   {
     sequelize,
