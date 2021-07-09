55dd77301fbdb633c2ca95c28394c23c7eab57fa
Lee Joo Hyung <wngud985@gmail.com>
2021-07-09 14:35:30 +0900

    [server] fix/mainpage fix donation amount added

--- controllers/mainpageController.ts
+++ controllers/mainpageController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { MainPosts } from "../models/MainPosts";
+import { Donors } from "../models/Donors";
 
 const mainpageController = {
   mainpageController: async (req: Request, res: Response) => {
@@ -10,10 +11,20 @@ const mainpageController = {
 
   mainpageSingleController: async (req: Request, res: Response) => {
     const post = await MainPosts.findByPk(req.params.id);
+
     if (!post) {
       return res.status(404).json({ msg: "Post not found" });
     }
     res.status(200).send({ data: post });
+  },
+
+  mainpageDonationController: async (req: Request, res: Response) => {
+    const { title } = req.body;
+    const totalAmount = await Donors.sum("donationAmount", {
+      where: { mainPost_title: title },
+    });
+    const percentage = Math.floor((+`${totalAmount}` / 1000000) * 100);
+    res.status(200).send({ data: { percentage, totalAmount } });
   },
 };
 
