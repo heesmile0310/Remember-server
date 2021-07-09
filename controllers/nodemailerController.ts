71fa9bf378d97022cadc44301ce521d930da6a3f
Kimseonghee <kimsunghee625@gmail.com>
2021-07-09 21:53:51 +0900

    [server] feat/receiptmail add receiptmail controller

--- controllers/nodemailerController.ts
+++ controllers/nodemailerController.ts
@@ -65,6 +65,193 @@ const nodemailerController = {
 
     res.status(200).send({ data: authenNum });
   },
+
+  sendreceiptController: async (req: Request, res: Response) => {
+    const { name, mail, title, amount } = req.body;
+
+    const today = String(new Date()).slice(0, 15);
+    const random = Math.floor(Math.random() * 100);
+
+    const email = nodemailer.createTransport({
+      //메일트랩
+      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 2525,
+      secure: false,
+      auth: {
+        user: process.env.service_email,
+        pass: process.env.service_pass,
+      },
+    });
+
+    const content = await email.sendMail({
+      from: "Remember-service@gmail.com",
+      to: `${mail}`,
+      subject: `[Remember] ${name}후원자님, 요청하신 증명서를 보내드립니다.`,
+      html: `
+      <head>
+          <meta charset="utf-8">
+          <title>A simple, clean, and responsive HTML ******* template</title>
+          <style>
+              .invoice-box {
+                  max-width: 800px;
+                  margin: auto;
+                  padding: 30px;
+                  border: 1px solid #eee;
+                  box-shadow: 0 0 10px rgba(0, 0, 0, 0.15);
+                  font-size: 16px;
+                  line-height: 24px;
+                  font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif;
+                  color: #555;
+              }
+
+              .invoice-box table {
+                  width: 100%;
+                  line-height: inherit;
+                  text-align: left;
+              }
+
+              .invoice-box table td {
+                  padding: 5px;
+                  vertical-align: top;
+              }
+
+              .invoice-box table tr td:nth-child(2) {
+                  text-align: right;
+              }
+
+              .invoice-box table tr.top table td {
+                  padding-bottom: 20px;
+              }
+
+              .invoice-box table tr.top table td.title {
+                  font-size: 45px;
+                  line-height: 45px;
+                  color: #333;
+              }
+
+              .invoice-box table tr.information table td {
+                  padding-bottom: 40px;
+              }
+
+              .invoice-box table tr.heading td {
+                  background: #eee;
+                  border-bottom: 1px solid #ddd;
+                  font-weight: bold;
+              }
+
+              .invoice-box table tr.details td {
+                  padding-bottom: 20px;
+              }
+
+              .invoice-box table tr.item td {
+                  border-bottom: 1px solid #eee;
+              }
+
+              .invoice-box table tr.item.last td {
+                  border-bottom: none;
+              }
+
+              .invoice-box table tr.total td:nth-child(2) {
+                  border-top: 2px solid #eee;
+                  font-weight: bold;
+              }
+
+              @media only screen and (max-width: 600px) {
+                  .invoice-box table tr.top table td {
+                      width: 100%;
+                      display: block;
+                      text-align: center;
+                  }
+
+                  .invoice-box table tr.information table td {
+                      width: 100%;
+                      display: block;
+                      text-align: center;
+                  }
+              }
+
+              /** RTL **/
+              .invoice-box.rtl {
+                  direction: rtl;
+                  font-family: Tahoma, 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif;
+              }
+
+              .invoice-box.rtl table {
+                  text-align: right;
+              }
+
+              .invoice-box.rtl table tr td:nth-child(2) {
+                  text-align: left;
+              }
+          </style>
+      </head>
+
+      <body class="fr-no-selection">
+          <div class="invoice-box">
+              <table cellpadding="0" cellspacing="0">
+                  <tbody>
+                      <tr class="top">
+                          <td colspan="2">
+                              <table>
+                                  <tbody>
+                                      <tr>
+                                          <td class="title">
+                                              <div style="text-align: center;"><img src="https://myfiles.space/user_files/91396_e966e7460d24c3e4/91396_custom_files/img1625833024.jpeg" style="width: 348px;"></div>
+                                          </td>
+                                          <td>후원 #: ${random}<br>Created: ${today}<br></td>
+                                      </tr>
+                                  </tbody>
+                              </table>
+                          </td>
+                      </tr>
+                      <tr class="information">
+                          <td colspan="2">
+                              <table>
+                                  <tbody>
+                                      <tr>
+                                          <td>Remember, Inc.<br><span style="color: rgb(67, 67, 67); font-family: Gulim; font-size: 14px; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400; letter-spacing: normal; orphans: 2; text-align: left; text-indent: 0px; text-transform: none; white-space: normal; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; background-color: rgb(255, 255, 255); text-decoration-thickness: initial; text-decoration-style: initial; text-decoration-color: initial; float: none; display: inline !important;">경기도 성남시 분당구 대왕판교로236번길 22</span></td>
+                                          <td><br>${name}<br>${mail}</td>
+                                      </tr>
+                                  </tbody>
+                              </table>
+                          </td>
+                      </tr>
+                      <tr class="heading">
+                          <td>지불 방식</td>
+                          <td>Check #</td>
+                      </tr>
+                      <tr class="details">
+                          <td>Online</td>
+                          <td>1000</td>
+                      </tr>
+                      <tr class="heading">
+                          <td>후원처</td>
+                          <td>후원 금액</td>
+                      </tr>
+                      <tr class="item">
+                          <td>${title} 후원</td>
+                          <td>₩${amount}</td>
+                      </tr>
+                      <tr class="item">
+                          <td><br></td>
+                          <td><br></td>
+                      </tr>
+                      <tr class="item last">
+                          <td><br></td>
+                          <td><br></td>
+                      </tr>
+                      <tr class="total">
+                          <td><br></td>
+                          <td>Total: ₩${amount}</td>
+                      </tr>
+                  </tbody>
+              </table>
+          </div>
+      </body>
+      `,
+    });
+  },
 };
 
 export default nodemailerController;
