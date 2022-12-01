c3ebd9408e267db65ce68ca6b348094057acdf87
Kimseonghee <kimsunghee625@gmail.com>
2022-12-02 01:50:28 +0900

    REFACTOR: server.ts & index.ts 분리

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import morgan from "morgan";
 
 // route 적용
 import landingpageRouter from "./routes/landingpage";
@@ -11,63 +12,36 @@ import postRouter from "./routes/post";
 import userInfoRouter from "./routes/userInfo";
 import withdrawRouter from "./routes/withdraw";
 
-const bodyParser = require("body-parser");
-
-const http = require("http");
-const PORT = process.env.DATABASE_PORT;
-const cookieParser = require("cookie-parser");
-
-class App {
-  public application: express.Application;
-
-  constructor() {
-    this.application = express();
-  }
-}
-
-const app = new App().application;
-app.use(bodyParser.json());
-const port = 80;
-const ip = "127.0.0.1";
-const server = http.createServer(app);
-server.listen(port);
-const corsOption = {
-  Headers: { "content-type": "application/json" },
-  origin: true,
-  method: ["post", "get", "delete", "options", "put"],
-  credentials: true,
-  preflightContinue: true,
+let corsOptions = {
+  origin: "*",
+  optionsSuccessStatus: 200,
 };
 
-app.use((req, res, next) => {
-  next();
-}, cors(corsOption));
-app.options(
-  "*",
-  (req, res, next) => {
-    next();
-  },
-  cors(corsOption)
-);
+const createApp = () => {
+  const app = express();
+  app.use(cors(corsOptions));
 
-// route 적용
-app.use("/", landingpageRouter);
-app.use("/", loginRouter);
-app.use("/", mailRouter);
-app.use("/", mainpageRouter);
-app.use("/", postRouter);
-app.use("/", userInfoRouter);
-app.use("/", withdrawRouter);
+  app.use(morgan());
+  app.use(express.json());
+  // app.use(routes);
+  app.use("/", landingpageRouter);
+  app.use("/", loginRouter);
+  app.use("/", mailRouter);
+  app.use("/", mainpageRouter);
+  app.use("/", postRouter);
+  app.use("/", userInfoRouter);
+  app.use("/", withdrawRouter);
 
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.use(cors(corsOption));
-app.use(express.json());
-app.get("/", (req, res) => {
-  res.status(200).send("hehe");
-});
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+  app.get("/", (req, res) => {
+    res.status(200).send("hehe");
+  });
+  app.use((err, req, res, next) => {
+    const { status, message } = err;
+    console.error(err);
+    res.status(status || 500).json({ message });
+  });
 
-export default app;
+  return app;
+};
+
+export default createApp;
