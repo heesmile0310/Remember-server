e92bc81ec3f8494586dc241cb166221445d0b3ed
Kimseonghee <kimsunghee625@gmail.com>
2022-12-06 23:29:18 +0900

    REFACTOR: index.ts 불필요한 주석 삭제 및 라우터 리팩 마무리

--- routes/landingpage.ts
+++
@@ -1,8 +0,0 @@
-import * as controller from "../controllers/controller";
-import express from "express";
-const landingpageRouter = express.Router();
-
-landingpageRouter.get("/quiz", controller.quiz.quizController);
-landingpageRouter.post("/today", controller.today.todayController);
-
-export default landingpageRouter;
--- routes/mail.ts
+++
@@ -1,8 +0,0 @@
-import * as controller from "../controllers/controller";
-import express from "express";
-const mailRouter = express.Router();
-
-mailRouter.post("/mail", controller.mail.sendAuthController);
-mailRouter.post("/mailreceipt", controller.mail.sendreceiptController);
-
-export default mailRouter;
