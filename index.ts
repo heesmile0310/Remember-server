5b735d055d07dd0c4ccf8e2cb9dc97a0d7eb1ad6
Kimseonghee <kimsunghee625@gmail.com>
2022-12-10 15:03:38 +0900

    FIX: 배포 에러 해결

--- index.ts
+++ index.ts
@@ -22,11 +22,6 @@ const createApp = () => {
   app.get("/", (req, res) => {
     res.status(200).send("hehe");
   });
-  app.use((err, req, res, next) => {
-    const { status, message } = err;
-    console.error(err);
-    res.status(status || 500).json({ message });
-  });
 
   return app;
 };
